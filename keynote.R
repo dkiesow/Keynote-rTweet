03488ac8a972add226dcf16875df468428c512cd
Damon Kiesow <damon@kiesow.net>
2019-08-28 17:37:30 -0500

    Update keynote.R
    Added lines 19-20 to fix a character encoding problem and render curly quotes correctly in the published Tweet.

--- keynote.R
+++ keynote.R
@@ -5,23 +5,27 @@
 ## load rtweet package
 library(rtweet)
 me <- rtweet:::home_user()
+
 ## Pull parameters from command line (first_status will be "yes" or "no" and provided from the AppleScript)
 args <- commandArgs(trailingOnly = TRUE)
+
 ## Check to make sure there are two parameters
 if (length(args) < 2) {
- stop("Two arguments must be supplied")
+  stop("Two arguments must be supplied")
 } else if (length(args) == 2) {
- keynote_status <- args[1]
- first_status <- args[2]
+  keynote_status <- args[1]
+  first_status <- args[2]
 }
+keynote_status = iconv(keynote_status, "UTF-8", "cp1252"); 
+keynote_status = gsub("(\x93|\x94)", "\"", keynote_status, perl = T)
 ## Send a new tweet if first_status is "Yes" and reply tweet if "No"
 ## If you neglect to set the "[first]" flag in Keynote it will thread from your most recent tweet
 ## rtweet uses "get_timeline" to pull the last 3 tweets from your account
 ## It then uses "reply_id" to pull your last tweet's "status_id" from the data table of "my_timeline"
 if (first_status == 'yes') {
- post_tweet(status = keynote_status) 
+  post_tweet(status = keynote_status) 
 } else {
- my_timeline <- get_timeline(user = me, 3) 
- reply_id <- my_timeline[1,2]
- post_tweet(status = keynote_status, in_reply_to_status_id = reply_id)
+  my_timeline <- get_timeline(user = me, 3) 
+  reply_id <- my_timeline[1, 2]
+  post_tweet(status = keynote_status, in_reply_to_status_id = reply_id)
 }
